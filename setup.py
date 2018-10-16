0b0bedb93aa8f871a74fa8f8d7212bf33e491fa1
Jerin Philip <jerin.philip@students.iiit.ac.in>
2018-10-16 23:22:59 +0530

    Manifest + Model files within package

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ VERSION = None
 # What packages are required for this module to be executed?
 REQUIRED = [
     # 'requests', 'maya', 'records',
+    'langdetect', 'sentencepiece'
 ]
 
 # What packages are optional?
