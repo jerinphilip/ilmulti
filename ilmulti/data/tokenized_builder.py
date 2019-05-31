0af56b3f33484209576ad012dfbe2a4b02a717c7
Jerin Philip <jerin.philip@research.iiit.ac.in>
2019-05-31 17:03:34 +0530

    Removing data

--- ilmulti/data/tokenized_builder.py
+++
@@ -1,9 +0,0 @@
-class Builder:
-    pass
-
-class TokenizedBuilder(Builder):
-    def __init__(self, dataset, out_path):
-        self.dataset = dataset
-        self.out_path = outpath
-        self.corpus = CachedCorpus()
-
