06a3c29ff6783a3114f7a4ad2172b7a097e6c1b7
Jerin Philip <jerin.philip@research.iiit.ac.in>
2019-06-01 10:35:58 +0530

    More changes to dataconfig

---
+++ ilmulti/dataconfig/__init__.py
@@ -0,0 +1,19 @@
+import os
+from collections import namedtuple
+
+DATASET_REGISTRY = {}
+def dataset_register(tag, splits):
+    def __inner(f):
+        DATASET_REGISTRY[tag] = (splits, f)
+        return f
+    return __inner
+
+DATA_ROOT = 'ilmt-data/consolidated'
+def data_abspath(sub_path):
+    path = os.path.join(DATA_ROOT, sub_path)
+    return path
+
+Corpus = namedtuple('Corpus', 'tag path lang')
+def sanity_check(collection):
+    for corpus in collection:
+        print(corpus)
--- ilmulti/dataconfig/collection.py
+++ ilmulti/dataconfig/collection.py
@@ -1,29 +1,13 @@
-import pandas as pd
-import os
-from collections import namedtuple
-
-def _f(sub_path):
-    root = 'consolidated'
-    path = os.path.join(root, sub_path)
-    return path
-
-
-DATASET_REGISTRY = {}
-def dataset_register(tag, splits):
-    def __inner(f):
-        DATASET_REGISTRY[tag] = (splits, f)
-        return f
-    return __inner
-
-
-Corpus = namedtuple('Corpus', 'tag path lang')
+from . import DATASET_REGISTRY
+from . import dataset_register, data_abspath
+from . import Corpus, sanity_check
 
 @dataset_register('iitb-hi-en', ['train', 'dev', 'test'])
 def IITB_meta(split):
     corpora = []
     for lang in ['en', 'hi']:
         sub_path = 'iitb-hi-en/parallel/{}.{}'.format(split, lang)
-        corpus = Corpus('iitb-hi-en', _f(sub_path), lang)
+        corpus = Corpus('iitb-hi-en', data_abspath(sub_path), lang)
         corpora.append(corpus)
     return corpora
 
@@ -38,7 +22,7 @@ def WAT_meta(split):
                     lang, split, src
             )
             corpus_name = 'wat-ilmpc-{}-{}'.format(lang, 'en')
-            corpus = Corpus(corpus_name, _f(sub_path), src)
+            corpus = Corpus(corpus_name, data_abspath(sub_path), src)
             corpora.append(corpus)
     return corpora
 
@@ -47,14 +31,11 @@ def UFALEnTam_meta(split):
     corpora = []
     for lang in ['en', 'ta']:
         sub_path = 'ufal-en-tam/{}.{}'.format(split, lang)
-        corpus = Corpus('ufal-en-tam', _f(sub_path), lang)
+        corpus = Corpus('ufal-en-tam', data_abspath(sub_path), lang)
         corpora.append(corpus)
     return corpora
 
 
-def sanity_check(collection):
-    for corpus in collection:
-        print(corpus)
 
 if __name__ == '__main__':
     def merge(*_as):
