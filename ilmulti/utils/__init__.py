36348c8d48fdc75b824e189588ea2a67d10b462b
Shashank Siripragada <siripragadashashank@gmail.com>
2020-06-27 23:03:54 +0530

    Fixing KeyError

--- ilmulti/utils/__init__.py
+++ ilmulti/utils/__init__.py
@@ -3,10 +3,11 @@ import os
 ILMULTI_DIR = os.path.join(os.environ['HOME'], '.ilmulti')
 
 # Handling a special case for supernova.
-if os.environ['HOSTNAME'] in ['fusor', 'supernova']:
-    print("{hostname} detected. Switching. Please check if ok.".format(hostname=os.environ['HOSTNAME']))
-    user = os.environ['USER']
-    ILMULTI_DIR = os.path.join('/home/{user}'.format(user=user), '.ilmulti')
+if 'HOSTNAME' in os.environ:
+    if os.environ['HOSTNAME'] in ['fusor', 'supernova']:
+        print("{hostname} detected. Switching. Please check if ok.".format(hostname=os.environ['HOSTNAME']))
+        user = os.environ['USER']
+        ILMULTI_DIR = os.path.join('/home/{user}'.format(user=user), '.ilmulti')
 
 from .language_utils import canonicalize, language_token
 from .download_utils import download_resources
