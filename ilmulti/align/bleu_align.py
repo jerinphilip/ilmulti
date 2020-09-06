8e84043a5b60540b0072dec743e9ee22b8bcffa1
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-09-06 12:32:07 +0530

    Matching BLEUAlign API to PIB crawl

--- ilmulti/align/bleu_align.py
+++ ilmulti/align/bleu_align.py
@@ -8,7 +8,7 @@ class BLEUAligner:
         self.tokenizer = tokenizer
         self.segmenter = segmenter
 
-    def __call__(self, src, src_lang, tgt, tgt_lang):
+    def __call__(self, src, src_lang, tgt, tgt_lang, galechurch=False):
         """
             Input: Paragraphs in two languages and their language codes.
             Output: Obtained parallel sentences using BLEUAlign
@@ -22,43 +22,57 @@ class BLEUAligner:
 
         def process(content, lang):
             lang, segments = self.segmenter(content, lang=lang)
-            tokenized, _io = create_stringio(segments, lang)
-            return tokenized, _io
+            missing_idxs = []
+            for idx, segment in enumerate(segments):
+                if not segment.strip():
+                    missing_idxs.append(idx)
 
-        src_tokenized, src_io = process(src, src_lang)
-        tgt_tokenized, tgt_io = process(tgt, tgt_lang)
+            tokenized, _io = create_stringio(segments, lang)
+            return tokenized, _io, missing_idxs
+
+        src_tokenized, src_io, missing_idxs = process(src, src_lang)
+        tgt_tokenized, tgt_io, _ = process(tgt, tgt_lang)
+
+        if galechurch==True:
+            src, tgt = self.bleu_align(src_io, tgt_io, hyp_src_tgt_file=None)
+            return ([], []) , (src, tgt)
 
         # Inject tokens into src_tokenized
-        injected_src_tokenized = inject_token(src_tokenized,tgt_lang)
+        injected_src_tokenized = inject_token(src_tokenized, tgt_lang)
 
-        # Processing using src_tokenized to get translations
-        # TODO(shashank) accumulate list
         generation_output = self.model(injected_src_tokenized)
-
         hyps = [ gout['tgt'] for gout in generation_output ]
 
-        #hyp_tokenized, hyp_io = create_stringio(hyps, tgt_lang)
+        # Remove missing idxs.
+        for idx in missing_idxs:
+            hyps[idx] = ''
+
         hyps = '\n'.join(hyps)
         hyp_io = StringIO(hyps)
 
-        return self.bleu_align(src_io, tgt_io, hyp_io)
+        src, tgt = self.bleu_align(src_io, tgt_io, hyp_io)
 
-    def bleu_align(self, srcfile, tgtfile, hyp_src_tgt_file):
+        return (src_tokenized, hyps), (src, tgt) 
+
+
+
+    def bleu_align(self, srcfile, tgtfile, hyp_src_tgt_file=None):
         output = StringIO()
-        # src_out, tgt_out = StringIO(), StringIO()
         options = {
-            'verbosity': 0,
             'srcfile': srcfile,
             'targetfile': tgtfile,
-            'srctotarget': [hyp_src_tgt_file],
+            'galechurch' : True if hyp_src_tgt_file is None else False,
+            'no_translation_override':True if hyp_src_tgt_file is None else False,
+            'srctotarget': [hyp_src_tgt_file] if hyp_src_tgt_file else [],
             'targettosrc': [],
-            # 'output': output,
-            # 'output-src': src_out, 'output-target': tgt_out,
-	}
+            'verbosity' : 0,
+	   }
 
         a = Aligner(options)
         a.mainloop()
         src_out, tgt_out = a.results()
+
         srcs = src_out.getvalue().splitlines()
         tgts = tgt_out.getvalue().splitlines()
+
         return srcs, tgts
