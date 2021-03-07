0b737e74844fe93c648a0d3e6ba53e34dffeceea
Jerin Philip <jerin.philip@research.iiit.ac.in>
2021-03-07 00:17:06 +0000

    Adding indiccorp 10M trained punkt

--- ilmulti/registry/splitters.py
+++ ilmulti/registry/splitters.py
@@ -27,6 +27,14 @@ def __punkt_pib():
     config = { lang: {'lang': lang, 'path': path(lang)} for lang in langs }
     return config
 
+@register_splitter('punkt/pib', MultiPunktSplitter)
+def __punkt_indiccorp():
+    ASSETS_DIR = resolve()
+    data_dir = os.path.join(ASSETS_DIR, 'punkt/indiccorp')   
+    path  = lambda lang: os.path.join(data_dir, '{}.pickle'.format(lang))
+    langs = [ "as", "bn", "en", "gu", "hi", "ml", "mr", "or", "pa", "ta", "te"]
+    config = { lang: {'lang': lang, 'path': path(lang)} for lang in langs }
+    return config
 
 if __name__ == '__main__':
     from . import registry
