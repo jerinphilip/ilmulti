44bc8239825b54f2011b7345854400e062479eab
Jerin Philip <jerin.philip@students.iiit.ac.in>
2021-03-08 01:28:49 +0530

    Changes to train script for larger corpus

--- ilmulti/cli/punkt_train.py
+++ ilmulti/cli/punkt_train.py
@@ -6,28 +6,54 @@ from ..ssplit.punkt import PunktDelimiter, inspect_tokenizer
 
 
 
-def train(lang, corpus, save_path):
+def train(args):
     # Refer:
     # https://github.com/alvations/DLTK/blob/84bb7daeda21c18424518731928aea103c15caa1/dltk/tokenize/tokenizer.py#L37
 
-    with open(corpus) as fp:
-        language_vars = PunktDelimiter(lang)()
+    def buffered_read(fp, num_lines, max_lines):
+        _buffer = []
+        iterator = iter(fp)
+        idx = 0
+        while True:
+            try:
+                line = next(iterator)
+            except UnicodeDecodeError:
+                continue
+
+            _buffer.append(line)
+            idx = idx + 1
+            if idx % num_lines == 0:
+                yield '\n'.join(_buffer)
+                _buffer = []
+            if idx >= max_lines:
+                break
+        yield '\n'.join(_buffer)
+
+
+    with open(args.train_corpus) as fp:
+        language_vars = PunktDelimiter(args.lang)()
         punkt = PunktTrainer(lang_vars=language_vars)
-        contents = fp.read()
-        punkt.train(contents, verbose=True, finalize=True)
+        # contents = fp.read()
+        iterator = buffered_read(fp, args.lines_buffer, args.max_lines) 
+        for idx, _buffer in enumerate(iterator, 1):
+            print("{idx} batches processed...".format(idx=idx))
+            punkt.train(_buffer, verbose=False, finalize=False)
+
         punkt.finalize_training(verbose=True)
         model = PunktSentenceTokenizer(punkt.get_params())
 
-        with open(save_path, 'wb+') as save_file:
+        with open(args.save_model, 'wb+') as save_file:
             pickle.dump(model, save_file, protocol=pickle.HIGHEST_PROTOCOL)
 
-    inspect_tokenizer(model)
+    # inspect_tokenizer(model)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--train-corpus', type=str, required=True)
     parser.add_argument('--save-model', type=str, required=True)
     parser.add_argument('--lang', type=str, required=True)
+    parser.add_argument('--lines-buffer', type=int, default=250000)
+    parser.add_argument('--max-lines', type=int, default=10**6)
     args = parser.parse_args()
-    train(args.lang, args.train_corpus, args.save_model)
+    train(args)
 
